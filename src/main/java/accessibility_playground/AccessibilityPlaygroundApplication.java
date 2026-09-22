5be91800a4d6c644e231c1c89a43af478aafa150
Crystal Shen <jshen214@gmail.com>
2026-09-22 10:53:43 -0700

    initial setup

---
+++ src/main/java/accessibility_playground/AccessibilityPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package accessibility_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessibilityPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessibilityPlaygroundApplication.class, args);
+	}
+
+}
